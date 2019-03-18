ba35cc8cd3077d134c3526f9af31621d5bb71493
Michael Weibel <michael.weibel@liip.ch>
2019-03-18 09:32:23 +0100

    Prettier on landingpage

--- landingpage/script.js
+++ landingpage/script.js
@@ -39,17 +39,17 @@ function clearInput(e) {
   e.target.value = "";
 }
 
-function appendToListElement (el, list) {
-  const li = document.createElement('li')
-  li.appendChild(el)
-  list.appendChild(li)
+function appendToListElement(el, list) {
+  const li = document.createElement("li");
+  li.appendChild(el);
+  list.appendChild(li);
 }
 
-function createErrorNode (message) {
-  const err = document.createElement('span')
-  err.className = 'red'
-  err.appendChild(document.createTextNode(message))
-  return err
+function createErrorNode(message) {
+  const err = document.createElement("span");
+  err.className = "red";
+  err.appendChild(document.createTextNode(message));
+  return err;
 }
 
 function onDocumentLoad() {
@@ -57,24 +57,28 @@ function onDocumentLoad() {
   const inputSubmit = document.querySelector(".input-submit");
   const ghReleasesSearch = document.querySelector(".gh-releases-search");
   const ghReleasesResult = document.querySelector(".gh-releases-results");
-  const headingReleasesResult = document.querySelector(".heading-releases-results");
+  const headingReleasesResult = document.querySelector(
+    ".heading-releases-results"
+  );
 
-  const listener = function (event) {
+  const listener = function(event) {
     event.preventDefault();
     event.stopPropagation();
 
     const url = inputRepo.value;
     if (url.length === 0) {
-      return
+      return;
     }
 
     const match = /(https?:\/\/github\.com\/)?([^/]+)\/([^/]+).*/i.exec(url);
     if (!match) {
-      removeAllChildren(ghReleasesResult)
+      removeAllChildren(ghReleasesResult);
 
-      const err = createErrorNode('URL incorrect. Example: https://github.com/rokka-io/rokka-go')
-      appendToListElement(err, ghReleasesResult)
-      url.value = '';
+      const err = createErrorNode(
+        "URL incorrect. Example: https://github.com/rokka-io/rokka-go"
+      );
+      appendToListElement(err, ghReleasesResult);
+      url.value = "";
 
       return;
     }
@@ -92,26 +96,28 @@ function onDocumentLoad() {
         Accept: "application/vnd.github.v3+json"
       }
     })
-      .then(function (response) {
+      .then(function(response) {
         if (!response.ok) {
           return Promise.reject(response);
         }
         return response.json();
       })
-      .then(function (response) {
+      .then(function(response) {
         const release = filterReleases(response);
         return release.assets;
       })
-      .then(function (assets) {
+      .then(function(assets) {
         if (!assets || !assets.length) {
           return Promise.reject(new Error("No asset found"));
         }
 
         removeAllChildren(ghReleasesResult);
-        removeAllChildren(headingReleasesResult)
-        headingReleasesResult.appendChild(document.createTextNode('Available Asset URLs'))
+        removeAllChildren(headingReleasesResult);
+        headingReleasesResult.appendChild(
+          document.createTextNode("Available Asset URLs")
+        );
 
-        assets.forEach(function (asset) {
+        assets.forEach(function(asset) {
           const path = `/gh/${organization}/${repo}/latest/${asset.name}`;
           const li = document.createElement("li");
           li.className = "pb2";
@@ -121,7 +127,7 @@ function onDocumentLoad() {
         });
         resetButton(inputSubmit);
       })
-      .catch(function (error) {
+      .catch(function(error) {
         resetButton(inputSubmit);
         removeAllChildren(ghReleasesResult);
 
@@ -129,13 +135,13 @@ function onDocumentLoad() {
         if (message === "Forbidden") {
           message = `${message} - most likely means that you exceeded the hourly rate limit, sorry. Try constructing the URL on your own please :)`;
         }
-        appendToListElement(createErrorNode(message), ghReleasesResult)
+        appendToListElement(createErrorNode(message), ghReleasesResult);
       });
 
     return false;
-  }
+  };
   ghReleasesSearch.addEventListener("submit", listener);
-  inputRepo.addEventListener('blur', listener);
+  inputRepo.addEventListener("blur", listener);
 }
 
 if (document.readyState === "loading") {
